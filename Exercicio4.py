cdf2cde40762dd606b315a08d1ff2f0870216acd
Paulo César <pc3168@gmail.com>
2022-11-01 00:40:03 -0300

    Alterando os exercícos 4 e 5

--- Exercicio4.py
+++ Exercicio4.py
@@ -1,5 +1,14 @@
 #4) Implementar uma função que retorne verdadeiro se o número for primo
 #(falso caso contrário). Testar de 1 a 100.
 
-for i in range(100):
-    print(i+1)
+
+def primo(n):
+    mult = 0
+    for i in range(2,n):
+        if( n % i == 0):
+            mult += 1
+    if(mult == 0 and n != 1):
+        print("{} É primo".format(n))
+
+for i in range(101):
+    primo(i+1)
--- Exercicio5.py
+++ Exercicio5.py
@@ -1,3 +1,16 @@
 #5) Escreva uma função que:
 #a) Receba uma frase como parâmetro.
 #b) Retorne uma nova frase com cada palavra com as letras invertidas.
+
+def primeiro_nome(nome):
+    return nome
+
+def inverter(texto):
+    return texto[::-1]
+
+name = input("Digite o primeiro nome: ")
+print("O nome digitado é {} ".format(primeiro_nome(name)))
+
+
+algumacoisa = input("Digite uma palavra para ser invertida: ")
+print(inverter(algumacoisa))
