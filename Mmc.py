f2e90e0af5cf1155302f9e26411cfa4a3c4e0649
Paulo César <pc3168@gmail.com>
2022-11-13 00:17:23 -0300

    CRIANDO O MÉTODO CALULA IMC

--- Mmc.py
+++ Mmc.py
@@ -1,14 +1,29 @@
 from tkinter import *
 
 from setuptools.command.saveopts import saveopts
-
+def calcula(imc):
+    if  imc < 17:
+        valor = "Muito abaixo do peso"
+    elif imc < 18.49:
+        valor = "Abaixo do peso"
+    elif imc < 24.99:
+        valor = "Peso normal"
+    elif imc < 29.99:
+        valor = "Acima do peso"
+    elif imc < 34.99:
+        valor = "Obesidade I"
+    elif imc < 39.99:
+        valor = "Obesidade II (severa)"
+    else:
+        valor = "Obesidade III (mórbida)"
+    return valor
 
 def calcular():
-    print("Nome: %s"% np.get())
-    print("Endereço: %s" % end.get())
-    print("Altura: %s" % alt.get())
-    print("peso: %s" % pe.get())
-    print("IMC: %s" % (float(pe.get()) / (float(alt.get())** 2)))
+    resultado.insert(INSERT, "Nome: %s"% np.get())
+    resultado.insert(INSERT, "\nEndereço: %s" % end.get())
+    resultado.insert(INSERT, "\nAltura: %s" % alt.get())
+    resultado.insert(INSERT, " e Peso: %s" % pe.get())
+    resultado.insert(END, "\nIMC: %s" % calcula((float(pe.get()) / (float(alt.get())** 2))))
 
 def reiniciar():
     np.delete(0,END)
@@ -21,8 +36,8 @@ def sair():
     app.destroy()
 
 app=Tk()
-app.title("MMC")
-app.geometry("400x250")
+app.title("Tabela de IMC")
+app.geometry("500x250")
 app.configure(background="#dde")
 
 Label(app, text="Nome:", anchor=W).place(x=10,y=10,width=100,height=20)
@@ -35,16 +50,16 @@ end=Entry(app)
 alt=Entry(app)
 pe=Entry(app)
 
-np.place (x=110,y=10,width=250,height=20)
-end.place(x=110,y=35,width=250,height=20)
+np.place (x=110,y=10,width=300,height=20)
+end.place(x=110,y=35,width=300,height=20)
 alt.place(x=110,y=60,width=100,height=20)
 pe.place (x=110,y=85,width=100,height=20)
 
 resultado=Text(app)
-resultado.place (x=220,y=60,width=140,height=100)
+resultado.place (x=220,y=60,width=190,height=100)
 
 Button(app, text="Calcular", command=calcular).place (x=10,y=180,width=100,height=30)
 Button(app, text="Reiniciar", command=reiniciar).place (x=110,y=180,width=100,height=30)
-Button(app, text="Sair", command=sair).place (x=260,y=180,width=100,height=30)
+Button(app, text="Sair", command=sair).place (x=310,y=180,width=100,height=30)
 
 app.mainloop()
